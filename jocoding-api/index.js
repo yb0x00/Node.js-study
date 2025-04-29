5713631efbc2f222819955916b626a9c540f6af7
yb0x00 <yeongbeen0x00@gmail.com>
2025-04-29 11:23:54 +0900

    feat: express를 활용한 API 서버 구축
    - 요청을 받아서 응답을 보내는 API
    - http://localhost:3000/ 실행하여 확인

--- jocoding-api/index.js
+++ jocoding-api/index.js
@@ -1,10 +1,19 @@
-var figlet = require("figlet");
+const express = require('express')
+const app = express()
+const port = 3000
 
-figlet("AnB", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
-});
+app.get('/', (req, res) => {    //HTTP 메소드 라우팅 콜백함수
+    res.send('Hello World!')
+})
+
+app.get('/dog', (req, res) => {
+    res.json({'sound':'멍멍'})
+})
+
+app.get('/cat', (req, res) => {
+    res.json({'sound':'야옹'})
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
